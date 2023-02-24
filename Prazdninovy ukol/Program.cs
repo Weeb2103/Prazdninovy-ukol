251bab6bdfa165db2e4150d4a1d68cff76f3d9a1
Jiří Vondráček <jiri.vondracek@educhem.cz>
2023-02-24 21:02:22 +0100

    Přidejte soubory projektu.

---
+++ Prazdninovy ukol/Program.cs
@@ -0,0 +1,25 @@
+﻿for (int i = 1; i < 101; i++)
+{
+    if (i== 40)
+    {
+        continue;
+    }
+    if (i ==60)
+    {
+        continue;
+    }
+    Console.WriteLine(i);
+}
+//zde končí první úkol
+Console.WriteLine("***************************");
+Console.WriteLine("*                         *");
+Console.WriteLine("*                         *");
+Console.WriteLine("*                         *");
+Console.WriteLine("***************************");
+//zde končí druhý úkol
+Console.WriteLine("Zadejte čísla oddělená čárkou:");
+string vstup = Console.ReadLine();
+string[] cislaJakoText = vstup.Split(',');
+int pocetCisel = cislaJakoText.Length;
+Console.WriteLine("Zadal(a) jste " + pocetCisel + " čísel.");
+//zde končí třetí úkol
